b354f9e2bdde880db24227c892d4210fbcb5e3f9
Jose David Castillo <jose1xdd@gmail.com>
2025-09-14 17:44:00 -0500

    asistencia individual

--- app/models/outputs/asistencia/asistencia_persona.py
+++ app/models/outputs/asistencia/asistencia_persona.py
@@ -3,3 +3,5 @@ from pydantic import BaseModel
 
 class AsistenciaIndividual(BaseModel):
     asistencia_persona: bool
+    class Config:
+        from_attributes = True  # Permite mapear desde SQLAlchemy
--- app/models/outputs/reunion/reunion_out.py
+++ app/models/outputs/reunion/reunion_out.py
@@ -11,4 +11,4 @@ class ReunionOut(BaseModel):
     horaFinal: Optional[time] = None
 
     class Config:
-        orm_mode = True
+        from_attributes = True
